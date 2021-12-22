58973de36af72065f005373c6d0c4b14b9c8c604
AcezukyRockon <tranghk@gmail.com>
2021-12-22 08:39:29 +0700

    leave only barebone files

--- spec/your-name-word-count-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import YourNameWordCountView from '../lib/your-name-word-count-view';
-
-describe('YourNameWordCountView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
