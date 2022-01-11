d8c5ffc390c4effe7e4cc8b59e5d771e16c977ec
hohaicongthuan <hohaicongthuan@gmail.com>
2022-01-11 20:37:30 +0700

    Add a new package

---
+++ spec/nhom1-atom-snippet-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Nhom1AtomSnippetView from '../lib/nhom1-atom-snippet-view';
+
+describe('Nhom1AtomSnippetView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
